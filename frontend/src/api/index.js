26960ef7819834d5d4aede19547ffee9c1dae479
iyedkh <111179698+Iyedkh@users.noreply.github.com>
2026-04-15 09:19:51 +0100

    deploy

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: process.env.NODE_ENV === 'production'
+    ? 'brand-production-f803.up.railway.app' 
+    : 'http://localhost:5000',
 });
 
 api.interceptors.request.use((config) => {
